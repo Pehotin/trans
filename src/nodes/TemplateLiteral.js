9ba2b0608fafc00789c0dc60f9372a82a3a99908
Mark P <mark.p.developer@gmail.com>
2023-07-10 15:45:48 +0300

    Add supported method template literal

--- src/nodes/TemplateLiteral.js
+++ src/nodes/TemplateLiteral.js
@@ -4,6 +4,42 @@ module.exports = class TemplateLiteral extends Node {
   meta = 'template-literal'
 
   transpile(chunk) {
+    if (!this.features.includes('templateString')) {
+      return this._transpileUnsupported(chunk)
+    }
+
+    return this._transpileSupported(chunk)
+  }
+
+  _transpileSupported(chunk) {
+    const nodes = []
+
+    nodes.push(...this.node.expressions)
+    nodes.push(...this.node.quasis)
+
+    nodes.sort((a, b) => {
+      return a.start - b.start
+    })
+
+    chunk.add('`')
+
+    nodes.forEach((node, i, arr) => {
+      const chunkChild = this.traverse(node).first()
+
+      if (chunkChild.hasMeta('template-element')) {
+        chunk.children([chunkChild])
+        return
+      }
+
+      chunk.add('${').children([chunkChild]).add('}')
+    })
+
+    chunk.add('`')
+
+    return chunk
+  }
+
+  _transpileUnsupported(chunk) {
     const nodes = []
 
     nodes.push(...this.node.expressions)
